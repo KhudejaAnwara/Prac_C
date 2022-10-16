051e2b994ce3f778a8133dadf4d674ba8c3a0e04
KhudejaAnwara <115945493+KhudejaAnwara@users.noreply.github.com>
2022-10-16 23:33:43 +0600

    Add files via upload

---
+++ Getchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define A 1.2
+char choice;
+int main()
+{
+    choice = getchar();
+    printf("This is a character- %c\n", choice);
+
+    printf("%.2f", A);
+    //int A = 3.1;
+    return 0;
+}
